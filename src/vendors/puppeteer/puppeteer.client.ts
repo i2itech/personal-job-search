30d290b70560aafd9e863a29cda3c8e762ea064b
Denzelle Walker <dwalker@jetlifetech.com>
2025-02-25 10:31:13 -0500

    fix: resume generation error

--- src/vendors/puppeteer/puppeteer.client.ts
+++ src/vendors/puppeteer/puppeteer.client.ts
@@ -9,7 +9,7 @@ export class PuppeteerClient {
     const browser = await puppeteer.launch({
       executablePath,
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: [...chromium.args, "--no-sandbox", "--disable-setuid-sandbox"],
     });
 
     try {

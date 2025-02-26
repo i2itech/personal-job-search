7711058fa5a1e83a0c7e1735d5897c605ba5ddf7
Denzelle Walker <dwalker@jetlifetech.com>
2025-02-26 12:13:05 -0500

    feat: updated company entity

--- src/shared/entities/company.entity.ts
+++ src/shared/entities/company.entity.ts
@@ -1,15 +1,31 @@
+import { NotionEntity, NotionEntityProperty } from "../../vendors/notion/notion-entity.decorator";
 import { ContactEntity } from "./contact.entity";
 
+@NotionEntity({
+  database_id: "ad56d3e62f9c4774bdc379f98fca4b9f",
+})
 export class CompanyEntity {
-  static DatabaseId: string = "ad56d3e62f9c4774bdc379f98fca4b9f";
   constructor(partial: Partial<CompanyEntity> = {}) {
     Object.assign(this, partial);
   }
 
+  @NotionEntityProperty({ type: "id" })
   id: string;
+
+  @NotionEntityProperty({ type: "title", notionKey: "Name" })
   name: string;
-  contacts?: ContactEntity[] = [];
+
+  @NotionEntityProperty({ type: "relation", notionKey: "Contacts" })
+  contact_ids?: string[];
+
+  @NotionEntityProperty({ type: "url", notionKey: "Website" })
   website_url?: string;
+
+  @NotionEntityProperty({ type: "url", notionKey: "LinkedIn" })
   linkedin_url?: string;
+
+  @NotionEntityProperty({ type: "checkbox", notionKey: "Is Draft" })
   is_draft?: boolean;
+
+  contacts?: ContactEntity[] = [];
 }
